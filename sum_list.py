bd24b0f32194eb321b39dfb4b90884bdaf80491d
prodhanm <mustafizur.prodhan@gmail.com>
2025-05-08 01:47:36 -0400

    the sum function works well with the main

--- sum_list.py
+++ sum_list.py
@@ -5,8 +5,14 @@ def sumOfList(li):
     return total
 
 def main():
-    # Example usage
-    list_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    list_num = []
+    while True:
+        num = int(input("Enter an element (-1 to stop): "))
+        if num == -1:
+            break
+        else:
+            list_num.append(num)
+    
     result = sumOfList(list_num)
     print(f"The sum of the list is {result}")
 
